166db05b3ac0ac0ed7a5b5a65ecef668f61fac8e
ktsim88 <526ktsim@frhsd.com>
2024-11-26 02:43:11 +0000

    added second row of cards with working button

--- script.js
+++ script.js
@@ -111,3 +111,57 @@ document.getElementById("startBtn").addEventListener("click", function () {
     `;
     this.style.display = 'none'
 });
+// round 2 = final round
+document.getElementById("nextRoundBtn").addEventListener("click", function () {
+    let roundContainer = document.getElementById("roundTwoContainer");
+    roundContainer.innerHTML = `
+      <div class="round-content row">
+      <h2>Final Round: Cooking Frenzy</h2>
+      <div class="col-12 col-md-4 mb-3 g-2">
+          <div class="card mx-auto" style="width: 18rem;">
+              <div class="card-body text-center">
+                  <h5 class="card-title">Rules</h5>
+                  <h6 class="card-subtitle mb-2 text-body-secondary">(Round 1)</h6>
+                  <p class="card-text">
+                      <ol>
+                          <li>You have to survive 2 rounds and you have a chance to win <i>$${userNum}</i></li>
+                          <li>You have to get top <i>${numTop}</i> in order to survive.</li>
+                          <li>If you are below the top <i>${numTop}</i>, you will be <strong>eliminated.</strong></li>
+                          <li>Those who rank top <i>${numRank}</i> will have special benefits in each round.</li>
+                          <li>And lastly...good luck!</li>
+                      </ol>
+                  </p>
+              </div>
+          </div>
+      </div>
+      <div class="col-12 col-md-4 mb-3 g-2">
+          <div class="card mx-auto" style="width: 18rem;">
+              <div class="card-body text-center">
+                  <h5 class="card-title">Your Results</h5>
+                  <p class="card-text">
+                      <ul>
+                      <li>You are up against <i>${numPeople}</i> people.</li>
+                      <li>Your current rank is <i>${userRank}</i></li>
+                      </ul>
+                  </p>
+              </div>
+          </div>
+      </div>
+      <div class="col-12 col-md-4 mb-3 g-2">
+          <div class="card mx-auto" style="width: 18rem;">
+              <div class="card-body text-center">
+                  <h5 class="card-title">Your Stats</h5>
+                  <h6 class="card-subtitle mb-2 text-body-secondary">(How to Play)</h6>
+                  <p class="card-text">
+                      <ul>
+                      <li>You are up against <i>${numPeople}</i> people.</li>
+                      <li>Your current rank is <i>${userRank}</i></li>
+                      </ul>
+                  </p>
+              </div>
+          </div>
+      </div>
+  </div>
+      `;
+      this.style.display = 'none'
+  });
